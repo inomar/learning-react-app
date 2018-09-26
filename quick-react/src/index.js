64f77ba8fda605864cb5d47cb7aa724010308810
MakotoInomata <inomar1486@gmail.com>
2018-09-26 12:29:25 +0900

    add time sample

--- quick-react/src/index.js
+++ quick-react/src/index.js
@@ -5,4 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+setInterval(() => {
+    ReactDOM.render(
+        <div>現在の時刻は{(new Date()).toLocaleString()}です</div>,
+        document.getElementById('now')
+    )
+})
 registerServiceWorker();
