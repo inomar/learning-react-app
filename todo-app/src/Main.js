a2445109a06a6ccf7c8dfe659af710d26c127683
MakotoInomata <inomar1486@gmail.com>
2018-10-03 00:47:16 +0900

    add Register component

--- todo-app/src/Main.js
+++ todo-app/src/Main.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Registraition from './Registration';
 
 export default class Main extends Component {
     render() {
         return(
-            <div>Hello World!!</div>
+            <React.Fragment>
+              <div>Hello World!!</div>
+              <Registraition />
+            </React.Fragment>
+            
         )
     }
 }
---
+++ todo-app/src/Registration.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Registration extends Component {
+
+    render() {
+        return(
+            <form>
+                <label htmlFor="add">TODO</label>
+                <input type="text" id="add" name="add" />
+                <button type="button">追加</button>
+            </form>
+        )
+    }
+}
